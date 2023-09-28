076f1163440a3ed6fe2319f6e91a6e44513b564f
afigjabbarli <afiqcabbarli96@gmail.com>
2023-09-27 23:42:09 -0700

    Register view completed

---
+++ E-Commerce Platform/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using E_Commerce_Platform.ViewModels.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_Platform.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]   
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(UserAddViewModel model)
+        {
+            return View();  
+        }
+    }
+}
