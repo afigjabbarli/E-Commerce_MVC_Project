195fa5f59e98ff34d9b9fc6df6ad7bc16049fe9a
afigjabbarli <afiqcabbarli96@gmail.com>
2023-10-12 11:33:12 -0700

    Error page added to Auth views...

--- E-Commerce Platform/Controllers/AuthController.cs
+++ E-Commerce Platform/Controllers/AuthController.cs
@@ -1,10 +1,16 @@
-﻿using E_Commerce_Platform.ViewModels.User;
+﻿using E_Commerce_Platform.DataBase;
+using E_Commerce_Platform.ViewModels.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce_Platform.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly ECommerceDBContext _dbContext;
+        public AuthController(ECommerceDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         [HttpGet]   
         public IActionResult Register()
         {
@@ -16,6 +22,11 @@ namespace E_Commerce_Platform.Controllers
             if (!ModelState.IsValid)
             {
                 return View(model);
+            }
+            if(_dbContext.Users.Any(u => u.Email == model.Email))
+            {
+                TempData["ErrorMessage"] = "This email address is already available in the system!!!";
+                return RedirectToAction("ErrorPage", "Auth");
             }
             return RedirectToAction("Index", "Home");
         }
@@ -29,6 +40,11 @@ namespace E_Commerce_Platform.Controllers
         public IActionResult Login(UserLoginViewModel Model)
         {
             return RedirectToAction("Index", "Home");   
+        }
+        [HttpGet]
+        public IActionResult ErrorPage()
+        {
+            return View();
         }
     }
 }
