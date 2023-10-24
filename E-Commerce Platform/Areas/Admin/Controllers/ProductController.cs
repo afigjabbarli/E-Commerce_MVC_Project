17fb963e4c0095bed808b0d1a4772b6cb19b9a8e
afigjabbarli <afiqcabbarli96@gmail.com>
2023-10-24 05:18:51 -0700

    Update UI succesfully completed...

--- E-Commerce Platform/Areas/Admin/Controllers/ProductController.cs
+++ E-Commerce Platform/Areas/Admin/Controllers/ProductController.cs
@@ -4,7 +4,6 @@ using E_Commerce_Platform.DataBase;
 using E_Commerce_Platform.DataBase.Models;
 using E_Commerce_Platform.Services.Abstracts;
 using Microsoft.AspNetCore.Mvc;
-using System.Net.WebSockets;
 
 namespace E_Commerce_Platform.Areas.Admin.Controllers
 {
@@ -41,10 +40,10 @@ namespace E_Commerce_Platform.Areas.Admin.Controllers
                     Categories = _DBContext.Categories.ToList(),
                     Sizes = _DBContext.Sizes.ToList(),
                     Colors = _DBContext.Colors.ToList(),
-                };   
-                return View(model); 
+                };
+                return View(model);
             }
-           
+
             Product newProduct = new Product
             {
                 Name = viewModel.Name,
@@ -54,12 +53,12 @@ namespace E_Commerce_Platform.Areas.Admin.Controllers
                 CreatedAt = DateTime.UtcNow
             };
 
-            if(viewModel.Image is not null)
+            if (viewModel.Image is not null)
             {
                 newProduct.PhysicalImageName = _fileService
                     .Upload(viewModel.Image, CustomUploadDirectories.Products);
             }
-                
+
             _DBContext.Products.Add(newProduct);
             ///Category start...
             foreach (var categoryID in viewModel.CategoryIds)
@@ -76,7 +75,7 @@ namespace E_Commerce_Platform.Areas.Admin.Controllers
                     CategoryId = category.Id,
                     Product = newProduct
                 };
-                _DBContext.CategoryProducts.Add(categoryProduct);   
+                _DBContext.CategoryProducts.Add(categoryProduct);
             }
             ///Category end...
             ///
@@ -84,7 +83,7 @@ namespace E_Commerce_Platform.Areas.Admin.Controllers
             foreach (var sizeID in viewModel.SizeIds)
             {
                 var size = _DBContext.Sizes.SingleOrDefault(s => s.Id == sizeID);
-                if(size is null)
+                if (size is null)
                 {
                     var sizes = _DBContext.Sizes.ToList();
                     ModelState.AddModelError("SizeIds", "Size not found");
@@ -103,21 +102,21 @@ namespace E_Commerce_Platform.Areas.Admin.Controllers
             foreach (var colorID in viewModel.ColorIds)
             {
                 var color = _DBContext.Colors.SingleOrDefault(c => c.Id == colorID);
-                if(color is null)
+                if (color is null)
                 {
                     var colors = _DBContext.Colors.ToList();
                     ModelState.AddModelError("ColorIds", "Color not found");
-                    return View(colors);    
+                    return View(colors);
                 }
                 var productColor = new ProductColor
                 {
                     ColorId = color.Id,
                     Product = newProduct
                 };
-                _DBContext.ProductColors.Add(productColor); 
+                _DBContext.ProductColors.Add(productColor);
             }
             ///Color end...
-            _DBContext.SaveChanges();   
+            _DBContext.SaveChanges();
             return RedirectToAction(nameof(Read));
         }
         [HttpGet("read")]
@@ -129,32 +128,148 @@ namespace E_Commerce_Platform.Areas.Admin.Controllers
         }
         public List<ProductListItemViewModel> ConvertProductsToViewModel(List<Product> products)
         {
-                List<ProductListItemViewModel> viewModels = new List<ProductListItemViewModel>();
+            List<ProductListItemViewModel> viewModels = new List<ProductListItemViewModel>();
 
-                foreach (var product in products)
+            foreach (var product in products)
+            {
+                ProductListItemViewModel viewModel = new ProductListItemViewModel
                 {
-                    ProductListItemViewModel viewModel = new ProductListItemViewModel
-                    {
-                        ID = product.Id,
-                        Name = product.Name,
-                        Description = product.Description,
-                        Price = product.Price,
-                        ImageUrl = _fileService.GetStaticFilesUrl(CustomUploadDirectories.Products, product.PhysicalImageName),
-                        Colors = _DBContext.ProductColors.Where(pc => pc.ProductId == product.Id).Select(pc => pc.Color).ToList(),
-                        Sizes = _DBContext.ProductSizes.Where(ps => ps.ProductId == product.Id).Select(ps => ps.Size).ToList(),
-                        Categories = _DBContext.CategoryProducts.Where(cp => cp.ProductId == product.Id).Select(cp => cp.Category).ToList(),
-                    };
+                    ID = product.Id,
+                    Name = product.Name,
+                    Description = product.Description,
+                    Price = product.Price,
+                    ImageUrl = _fileService.GetStaticFilesUrl(CustomUploadDirectories.Products, product.PhysicalImageName),
+                    Colors = _DBContext.ProductColors.Where(pc => pc.ProductId == product.Id).Select(pc => pc.Color).ToList(),
+                    Sizes = _DBContext.ProductSizes.Where(ps => ps.ProductId == product.Id).Select(ps => ps.Size).ToList(),
+                    Categories = _DBContext.CategoryProducts.Where(cp => cp.ProductId == product.Id).Select(cp => cp.Category).ToList(),
+                };
 
-                    viewModels.Add(viewModel);
-                }
+                viewModels.Add(viewModel);
+            }
 
-                return viewModels;
+            return viewModels;
         }
+        [HttpGet("{id}/update")]
+        public IActionResult Update(int id)
+        {
+            var product = _DBContext.Products.SingleOrDefault(p => p.Id == id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+            var colors = _DBContext.ProductColors.Where(pc => pc.ProductId == product.Id).Select(product => product.Color).ToList();
+            var sizes = _DBContext.ProductSizes.Where(ps => ps.ProductId == product.Id).Select(product => product.Size).ToList();
+            var categories = _DBContext.CategoryProducts.Where(cp => cp.ProductId == product.Id).Select(product => product.Category).ToList();
+            var model = new ProductUpdateViewModel
+            {
+                Colors = colors,
+                Sizes = sizes,
+                Categories = categories,
+                ProductId = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                ImageUrl = _fileService.GetStaticFilesUrl(CustomUploadDirectories.Products, product.PhysicalImageName),
+                NewColors = _DBContext.Colors.ToList(),
+                NewSizes = _DBContext.Sizes.ToList(),
+                NewCategories = _DBContext.Categories.ToList()
+            };
+            return View(model);
+        }
+        //[HttpPost("update")]
+        //public IActionResult Update(ProductAddViewModel model)
+        //{
+        //    if(!ModelState.IsValid)
+        //    {
+        //        return View(model);
+        //    }
+                
+        //    var product = _DBContext.Products.SingleOrDefault(p => p.Id == model.ProductId);    
+        //    if(product is null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    if(model.Image is not null)
+        //    {
+        //        product.PhysicalImageName = _fileService.Upload(model.Image, CustomUploadDirectories.Products); ;   
+        //    }
+        //    product.Name = model.Name;
+        //    product.Description = model.Description;    
+        //    product.Price = model.Price;    
+        //    product.UpdatedAt = DateTime.UtcNow;
+
+        //    #region Color Addition
+
+        //    foreach (var colorID in model.ColorIds)
+        //    {
+        //        var color = _DBContext.Colors.SingleOrDefault(c => c.Id == colorID);
+        //        if(color is null)
+        //        {
+        //            var colors = _DBContext.Colors.ToList();
+        //            ModelState.AddModelError("ColorIds", "Color not found");
+        //            return View(colors);
+        //        }
+        //        var productColor = new ProductColor
+        //        {
+        //            ColorId = color.Id,
+        //            ProductId = product.Id,
+        //        };
+        //        _DBContext.ProductColors.Add(productColor); 
+        //    }
+
+        //    #endregion
+
+        //    #region Size Addition
+
+        //    foreach (var sizeID in model.SizeIds)
+        //    {
+        //        var size = _DBContext.Sizes.SingleOrDefault(s => s.Id == sizeID);
+        //        if(size is null)
+        //        {
+        //            var sizes = _DBContext.Sizes.ToList();
+        //            ModelState.AddModelError("SizeIds", "Szie not found");
+        //            return View(sizes);
+        //        }
+        //        var productSize = new ProductSize
+        //        {
+        //            SizeId = size.Id,
+        //            ProductId = product.Id
+        //        };
+        //        _DBContext.ProductSizes.Add(productSize);   
+        //    }
+
+        //    #endregion
+
+
+        //    #region Category Addition
+
+        //    foreach(var categoryID in model.CategoryIds)
+        //    {
+        //        var category = _DBContext.Categories.SingleOrDefault(ct => ct.Id == categoryID);
+        //        if (category is null)
+        //        {
+        //            var categories = _DBContext.Categories.ToList();
+        //            ModelState.AddModelError("CategoryIds", "Category not found");
+        //            return View(categories);
+        //        }
+        //        var categoryProduct = new CategoryProduct
+        //        {
+        //            CategoryId = category.Id,
+        //            ProductId = product.Id
+        //        };
+
+        //        _DBContext.CategoryProducts.Add(categoryProduct);   
+        //    }
+
+        //    #endregion
+
+        //    return View();
+        //}
         [HttpPost("{id}/delete")]
         public IActionResult Delete(int id)
         {
             var product = _DBContext.Products.Where(product => product.Id == id).SingleOrDefault();
-            if(product is null)
+            if (product is null)
             {
                 return NotFound();
             }
---
+++ E-Commerce Platform/Areas/Admin/ViewModels/Product/ProductUpdateViewModel.cs
@@ -0,0 +1,41 @@
+﻿using E_Commerce_Platform.DataBase.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce_Platform.Areas.Admin.ViewModels.Product
+{
+    public class ProductUpdateViewModel
+    {
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Product description is required.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Product price is required.")]
+
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "At least one category must be selected.")]
+        public int[] CategoryIds { get; set; }
+        public List<Category>? Categories { get; set; }
+
+        [Required(ErrorMessage = "At least one size must be selected.")]
+        public int[] SizeIds { get; set; }
+        public List<Size>? Sizes { get; set; }
+
+        [Required(ErrorMessage = "At least one color must be selected.")]
+        public int[] ColorIds { get; set; }
+        public List<Color>? Colors { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "You must add a product image.")]
+        public IFormFile Image { get; set; }
+
+        public List<Color>? NewColors { get; set; }
+        public List<Size>? NewSizes { get; set; }
+        public List<Category>? NewCategories { get; set; }
+    }
+}
